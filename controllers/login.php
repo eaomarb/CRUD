62703455531a7d63d77fbb1e9c94d42711dba4cc
omar <eaomarb@gmail.com>
2024-05-03 01:08:10 +0200

    .

--- controllers/login.php
+++ controllers/login.php
@@ -1,27 +1,40 @@
 <?php
 session_start();
-$base = '../'; // Ruta base per a la inclusió d'arxius
-require_once $base . 'db.php'; // Inclou l'arxiu de connexió a la base de dades
+$base = '../';  // Ruta base per a la inclusió d'arxius
+require_once $base . 'db.php';  // Inclou l'arxiu de connexió a la base de dades
 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['usuari']) && isset($_POST['contrasenya'])) {
         $usuari = $_POST['usuari'];
         $contrasenya = $_POST['contrasenya'];
 
-        $stmt = $conn->prepare("SELECT * FROM usuaris WHERE Usuari = :usuari");
+        $stmt = $conn->prepare('SELECT * FROM usuaris WHERE Usuari = :usuari');
         $stmt->bindParam(':usuari', $usuari);
         $stmt->execute();
 
         $usuari_db = $stmt->fetch(PDO::FETCH_ASSOC);
 
+        $stmt = $conn->prepare('SELECT Administrador FROM usuaris WHERE Usuari = :usuari');
+        $stmt->bindParam(':usuari', $usuari);
+        $stmt->execute();
+
+        $isAdmin = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $stmt = $conn->prepare('SELECT Editor FROM usuaris WHERE Usuari = :usuari');
+        $stmt->bindParam(':usuari', $usuari);
+        $stmt->execute();
+
+        $isEditor = $stmt->fetch(PDO::FETCH_ASSOC);
+
         if ($usuari_db && password_verify($contrasenya, $usuari_db['Contrasenya'])) {
             $_SESSION['loggedin'] = true;
             $_SESSION['username'] = $usuari_db['Usuari'];
+            $_SESSION['isAdmin'] = $isAdmin;
+            $_SESSION['isEditor'] = $isEditor;
             header('Location: ../index.php');
             exit;
         } else {
-            $error = "Usuari o contrasenya incorrectes.";
+            $error = 'Usuari o contrasenya incorrectes.';
             header('Location: login.php?error=' . urlencode($error));
             exit;
         }
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Aquest arxiu espera 3 tipus de peticions:
  * - Peticions sense cap paràmetre: retornarà la vista principal (MainView.php)
@@ -7,62 +8,46 @@
  *
  * En funció de la petició, farà unes crIdes o unes altres al controlador (AlumneController.php)
  */
+require ('controllers/botiga.php');
 
- require("controllers/botiga.php");
+if ($_SESSION['isEditor']) {
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'delete') {
+            if (isset($_GET['id'])) {
+                deleteProductes($_GET['id']);
+            }
 
-if (isset($_GET['action'])) 
-{
-    if ($_GET['action'] == 'delete') 
-    {
-        if (isset($_GET['id'])) {
-            deleteProductes($_GET['id']);
+            loadMainView();
+        } elseif ($_GET['action'] == 'new') {
+            loadNewProductesView();
+        } elseif ($_GET['action'] == 'edit') {
+            if (isset($_GET['id'])) {
+                loadEditProductesView($_GET['id']);
+            }
+        } elseif ($_GET['action'] == 'show') {
+            if (isset($_GET['id'])) {
+                loadShowProductesView($_GET['id']);
+            }
+        } else {
+            loadMainView();
         }
-
-        loadMainView();
-    } 
-    elseif ($_GET['action'] == 'new') 
-    {
-        loadNewProductesView();
-    } 
-    elseif ($_GET['action'] == 'edit') 
-    {
-        if (isset($_GET['id'])) 
-        {
-            loadEditProductesView($_GET['id']);
+    } elseif (isset($_POST['action'])) {
+        if ($_POST['action'] == 'add') {
+            $msg = null;
+            if (isset($_POST['Nom']) && isset($_POST['Preu']) && isset($_POST['Stock']) && isset($_POST['Mides']) && isset($_POST['Descripció']) && isset($_POST['Imatge'])) {
+                $msg = addProductes($_POST['Nom'], $_POST['Preu'], $_POST['Stock'], $_POST['Mides'], $_POST['Descripció'], $_POST['Imatge']);
+            }
+            loadMainView($msg);
+        } elseif ($_POST['action'] == 'up') {
+            if (isset($_POST['Id']) && isset($_POST['Nom']) && isset($_POST['Preu']) && isset($_POST['Stock']) && isset($_POST['Mides']) && isset($_POST['Descripció']) && isset($_POST['Imatge'])) {
+                upProductes($_POST['Id'], $_POST['Nom'], $_POST['Preu'], $_POST['Stock'], $_POST['Mides'], $_POST['Descripció'], $_POST['Imatge']);
+            }
+            loadMainView();
+        } else {
+            loadMainView();
         }
-    } 
-    elseif ($_GET['action'] == 'show') 
-    {
-        if (isset($_GET['id'])) 
-        {
-            loadShowProductesView($_GET['id']);
-        }
-    } 
-    else 
-    {
-        loadMainView();
     }
-} 
-elseif (isset($_POST['action'])) 
-{
-    if ($_POST['action'] == 'add') 
-    {
-        $msg = null;
-        if (isset($_POST['Nom']) && isset($_POST['Preu']) && isset($_POST['Stock']) && isset($_POST['Mides']) && isset($_POST['Descripció']) && isset($_POST['Imatge'])) {
-            $msg = addProductes($_POST['Nom'], $_POST['Preu'], $_POST['Stock'], $_POST['Mides'], $_POST['Descripció'], $_POST['Imatge']);
-        }
-        loadMainView($msg);
-    } elseif ($_POST['action'] == 'up') {
-        if (isset($_POST['Id']) && isset($_POST['Nom']) && isset($_POST['Preu']) && isset($_POST['Stock']) && isset($_POST['Mides']) && isset($_POST['Descripció']) && isset($_POST['Imatge'])) {
-            upProductes($_POST['Id'], $_POST['Nom'], $_POST['Preu'], $_POST['Stock'], $_POST['Mides'], $_POST['Descripció'], $_POST['Imatge']);
-        }
-        loadMainView();
-    }else {
-        loadMainView();
-    }
-} 
-else 
-{
+} else {
     loadMainView();
 }
 ?>
