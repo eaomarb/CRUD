c21150e94cdab1b7a499c1c371a5e16effa12b29
eaomarb <150187124+eaomarb@users.noreply.github.com>
2024-05-02 19:25:49 +0200

    login

---
+++ views/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LOGIN</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <form class="login-form" action="login.php" method="post">
+                <h2>Login</h2>
+                <?php if (isset($_GET['error'])) { ?>
+                    <p class="error"><?php echo $_GET['error']; ?></p>
+                <?php } ?>
+                <div class="form-group">
+                    <label for="uname">Usuari</label>
+                    <input type="text" name="uname" id="uname" class="form-control" placeholder="usuari" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Contrasenya</label>
+                    <input type="password" name="password" id="password" class="form-control" placeholder="contrasenya" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
