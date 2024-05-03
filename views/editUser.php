d2ee016b1af6114cdcc93be66788eefda651d322
frannzg <148803161+frannzg@users.noreply.github.com>
2024-05-03 16:21:28 +0200

    y

---
+++ views/editUser.php
@@ -0,0 +1,103 @@
+<?php
+var_dump($_SESSION);
+$result = []; // Inicializar como un array vacío
+
+?>
+<!doctype html>
+<html lang="ca">
+
+<head>
+    <!-- Metadades necessàries -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/main.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <title>Administració d'Usuari</title>
+</head>
+
+<body>
+    <div class="btn float-right">
+        
+        <?php
+            // Verificar si el usuario ha iniciado sesión
+            $isEditor = 0;
+            $isAdmin = 0;
+            if (isset($_SESSION['username'])) {
+                $username = $_SESSION['username'];
+                $isAdmin = $_SESSION['isAdmin'];
+                $isEditor = $_SESSION['isEditor'];
+                echo '<a href="./controllers/logout.php" class="btn btn-danger">Tancar sesio</a>';
+                echo '<p>Benvingut, ' . $username . '</p>';
+            } else {
+                echo '<a href="views/login.php" class="btn btn-primary mr-2">Login</a>';
+                echo '<a href="views/register.php" class="btn btn-secondary">Register</a>';
+            }
+
+            if ($isAdmin == 1){
+                echo '<a href="views/editUser.php" class="btn btn-primary mr-2">Editar usuaris</a>';
+            }
+
+        ?>
+    </div>
+    <div class="container">
+        <div class="py-5 text-center">
+            <h1>Administració</h1>
+            <h3>Control d'Usuaris</h3>
+        </div>
+
+        <?php
+            // Comprovar si hi ha un missatge de success
+            if (isset($missatge['Success'])) {
+                echo "<div class='alert alert-success' role='alert'>{$missatge['Success']}</div>";
+            } elseif (isset($missatge['Error'])) {
+                // Comprovar si hi ha un missatge d'error
+                echo "<div class='alert alert-danger' role='alert'>{$missatge['Error']}</div>";
+            }
+        ?>
+
+
+<div class="table-responsive-sm">
+				<table class="table table-striped">
+					<thead class="thead-dark">
+						<tr>
+							<th class="align-middle">ID</th>
+							<th class="align-middle">USUARI</th>
+							<th class="align-middle">CONSTRASENYA</th>
+							<th class="align-middle">PERMÍS ADMINISTRADOR</th>
+                            <th class="align-middle">PERMÍS EDITOR</th>
+							<th class="align-middle text-right"><a class="btn btn-primary" role="button" href="?action=new">Afegir</a></th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+							foreach($result as $row) {
+								echo "<tr>";
+								echo "<td class='align-middle'>" . $row['Id'] . "</td>";
+								echo "<td class='align-middle'>" . $row['Usuari'] . "</td>";
+								echo "<td class='align-middle'>" . $row['Contrasenya'] . "</td>";
+                                echo "<td class='align-middle'>" . $row['Administrador'] . "</td>";
+                                echo "<td class='align-middle'>" . $row['Editor'] . "</td>";
+								echo "<a class='btn btn-success' role='button' href='?action=show&id=".$row['id']."'>Mostrar</a> ";
+								echo "<a class='btn btn-warning' role='button' href='?action=edit&id=".$row['id']."'>Editar</a> ";
+								echo "<a class='btn btn-danger' role='button' href='?action=delete&id=".$row['id']."'>Eliminar</a> ";
+								echo "</td>";
+								echo "</tr>";
+							}
+						?>
+					</tbody>
+				</table>
+			</div>
+
+        <?php
+            if ($isEditor == 1) {
+                echo '<div class="text-center">';
+                echo '<a href="?action=new" class="btn btn-success btn-lg">Afegir un nou producte</a>';
+                echo '</div>';
+            }
+        ?>
+    </div>
+
+</body>
+
+</html>
